70a616ed266e21df20838ab383c94316e2fc39f2
mysteryhobo <thepeterlittle@gmail.com>
2020-09-25 20:13:19 -0400

    added array of strings to lengths problem

---
+++ New/Problems/arrayOfStringsToLengths.js
@@ -0,0 +1,9 @@
+
+
+
+const stringsToLengths = (arrayOfStrings) => {
+  return arrayOfStrings.map((string) => string.length);
+}
+
+
+console.log('test: ', stringsToLengths(['test', 'wow', 'much', 'amaze']));
